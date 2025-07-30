964c266e3fb72d4ce0233ccbc0e7019bfca5d9d0
jaimeardp <jaimeardp@gmail.com>
2025-07-30 08:03:29 -0500

    refactor merge_into_raw.sql to use single quotes for parameter placeholders

--- dags/sql/merge_into_raw.sql
+++ dags/sql/merge_into_raw.sql
@@ -1,10 +1,10 @@
 MERGE self_raw_zone.customer_events_raw T
 USING (
   SELECT * FROM self_staging_zone.customer_events_ext
-  WHERE year  = {{ params.p_year }}
-    AND month = {{ params.p_month }}
-    AND day   = {{ params.p_day }}
-    AND hour  = {{ params.p_hour }}
+  WHERE year  = '{{ params.p_year }}'
+    AND month = '{{ params.p_month }}'
+    AND day   = '{{ params.p_day }}'
+    AND hour  = '{{ params.p_hour }}'
 ) S
 ON  T.event_uuid = S.event_uuid
 WHEN NOT MATCHED THEN
