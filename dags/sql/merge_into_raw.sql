2c752fff2218ac4fe35046bc8cf1534ed67af53b
jaimeardp <jaimeardp@gmail.com>
2025-07-30 09:02:56 -0500

    ingestion_ts casting to timestamp

--- dags/sql/merge_into_raw.sql
+++ dags/sql/merge_into_raw.sql
@@ -20,7 +20,7 @@ USING (
         antiguedad_cliente_meses,
         score_crediticio,
         origen_captacion,
-        ingestion_ts,
+        SAFE.PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%S.%f', ingestion_ts) AS ingestion_ts,
         event_uuid,
         source_file
     FROM  `self_staging_zone.customer_events_ext`
