4ce9a5b7251da26c17ad5bd32fb3ff3be6ca04d7
jaimeardp <jaimeardp@gmail.com>
2025-07-30 17:30:08 -0500

    literal to string

--- dags/sql/merge_into_raw.sql
+++ dags/sql/merge_into_raw.sql
@@ -27,10 +27,10 @@ USING (
         event_uuid,
         source_file
     FROM  `self_staging_zone.customer_events_ext`
-    WHERE year  = {{ data_interval_start.strftime("%Y") }}
-      AND month = {{ data_interval_start.strftime("%m") }}
-      AND day   = {{ data_interval_start.strftime("%d") }}
-      AND hour  = {{ data_interval_start.strftime("%H") }}
+    WHERE year  = '{{ data_interval_start.strftime("%Y") }}'
+      AND month = '{{ data_interval_start.strftime("%m") }}'
+      AND day   = '{{ data_interval_start.strftime("%d") }}'
+      AND hour  = '{{ data_interval_start.strftime("%H") }}'
 ) AS S
 ON T.event_uuid = S.event_uuid        -- idempotent key
 AND T.ingestion_ts BETWEEN
